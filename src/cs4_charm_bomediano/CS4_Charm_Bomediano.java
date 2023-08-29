d9689546b13897fb17f5cf3eddc84a4d30677753
CHARM <CHARM@172.22.16.73>
2023-08-29 16:14:12 +0800

    first java project

---
+++ src/cs4_charm_bomediano/CS4_Charm_Bomediano.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4_charm_bomediano;
+
+/**
+ *
+ * @author CHARM
+ */
+public class CS4_Charm_Bomediano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String greeting = "Hello World";
+}   
+    
+}
